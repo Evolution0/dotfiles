870e9c7c0ef0790f5179db7c48a895c62505b24e
Anthony Forsberg <forsberganthony@yahoo.com>
2013-09-05 13:26:19 -0400

    Added B.A.R and evilvte configs

---
+++ config.def.h
@@ -0,0 +1,31 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  20
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   1400
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 20
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+#define BAR_FONT       "-*-terminusmod,icons-medium-*-normal-*-12-*-*-*-*-*-iso8859-*","fixed"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Define the opacity of the bar (requires a compositor such as compton) */
+#define BAR_OPACITY 1.0 /* 0 is invisible, 1 is opaque */
+/* Color palette */
+#define BACKGROUND 0x1C1C23
+#define COLOR0 0x323246
+#define COLOR1 0x5A7882
+#define COLOR2 0x8C8CA0
+#define COLOR3 0x1E828C
+#define COLOR4 0x3C788C
+#define COLOR5 0x6EA0B4
+#define COLOR6 0x6E8CA0
+#define COLOR7 0x96BEC8
+#define COLOR8 0x323246
+#define COLOR9 0x5A7882
+#define FOREGROUND 0x9BBBC6
